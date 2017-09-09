60da872c15c9ae91d14d1b6d7f1ed0b46bbcd492
TenzenIga <arhan-93@list.ru>
2017-09-09 16:37:59 +0300

    fixed delete request

--- app.js
+++ app.js
@@ -43,6 +43,9 @@ app.use('/', poll);
 app.get('/*',(req,res)=>{
   res.sendFile(path.join(__dirname + '/public/index.html'));
 })
+app.get('/*/:*',(req,res)=>{
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+})
 
 app.listen(port, ()=>{
   console.log('Server started on port '+ port);
